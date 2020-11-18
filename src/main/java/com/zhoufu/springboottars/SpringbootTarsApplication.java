d06059a31bd83bfc6aaad0615069a3fddfd8a62f
zhoufu <zhoufu@51zhaoyou.com>
2020-11-19 01:34:27 +0800

    -A   项目

---
+++ src/main/java/com/zhoufu/springboottars/SpringbootTarsApplication.java
@@ -0,0 +1,13 @@
+package com.zhoufu.springboottars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhoufu/springboottars/SpringbootTarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhoufu.springboottars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
